5e35092795409819006d968301ec18076f2b94dd
allencarne <allencarne@yahoo.com>
2021-03-15 20:16:46 -0700

    Uploaded Project
    Updated Git Ignore
    
    Uploaded Unity Project
    *Tile Palette
    *Player Movement
    *Player Movement Animations

---
+++ FUUUUUUCK/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movement = moveInput.normalized * moveSpeed;
+        Animate();
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+    }
+
+    void Animate()
+    {
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+}
