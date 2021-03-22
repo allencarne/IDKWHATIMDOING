8cde5dffa15c2b06d81e8ddf86a0a33d2ed9ff4b
allencarne <allencarne@yahoo.com>
2021-03-22 16:39:44 -0700

    Basic Combat Animations
    Animate the charter Punch in 4 directions

---
+++ FUUUUUUCK/Assets/PlayerCombat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        // Play an Attack Animation
+        animator.SetTrigger("Attack");
+        // Detect Enemies
+
+        
+    }
+}
